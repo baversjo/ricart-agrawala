5baf0500c2ab7aa19878d26fbdeb39634a20e479
Johan Baversjo <baversjo@gmail.com>
2014-05-15 16:46:16 -0700

    vector clock implementation

--- src/ui/VectorClock.java
+++ src/ui/VectorClock.java
@@ -3,6 +3,7 @@ package ui;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class VectorClock implements Serializable{
@@ -24,5 +25,48 @@ public class VectorClock implements Serializable{
 	
 	public void increment(String pid){
 		vclock.put(pid,vclock.get(pid) + 1);
+	}
+	
+	public boolean lowerThan(VectorClock other){
+		for (Entry<String, Integer> entry : vclock.entrySet()) {
+			String pid = entry.getKey();
+			if(!other.vclock.containsKey(pid)){
+				other.vclock.put(pid, 0); //zero is default value
+			}
+		}
+		
+		for (Entry<String, Integer> entry : other.vclock.entrySet()) {
+			String pid = entry.getKey();
+			if(!vclock.containsKey(pid)){
+				vclock.put(pid, 0); //zero is default value
+			}
+		}
+		
+		boolean hasBigger = false;
+		boolean hasAtLeastOneSmaller = false;
+		
+		for (Entry<String, Integer> entry : vclock.entrySet()) {
+			String pid = entry.getKey();
+			int val1 = entry.getValue();
+			int val2 = other.vclock.get(pid);
+			if(val1 > val2){
+				hasBigger = true;
+				break;
+			}
+			if(val1 < val2){
+				hasAtLeastOneSmaller = true;
+			}
+			
+		}
+		
+		return hasAtLeastOneSmaller && !hasBigger;
+	}
+	
+	public VectorClock copy(){
+		VectorClock newClock = new VectorClock();
+		for(Entry<String, Integer> entry : vclock.entrySet()){
+			vclock.put(entry.getKey(), entry.getValue());
+		}
+		return newClock;
 	}
 }
